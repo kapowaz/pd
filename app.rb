d882a4155f7cc4b3a5688e6cc0820c6be3fddbcb
Sam Levy <git@levy.io>
2016-12-03 01:47:57 +0000

    error push

--- app.rb
+++ app.rb
@@ -1,26 +1,37 @@
 require './boot'
 
-client = NP::Client.new(
-  game_id: ENV['GAME_ID'],
-  auth: ENV['AUTH'],
-)
-response = client.get
+begin
 
-fleets = response.fleets.select { |f| f.player_id.to_s != ENV['PLAYER_ID'] }
-
-fleets.each do |fleet|
-  next if fleet.orders.empty?
-
-  order = fleet.orders.first
-
-  next if order.destination_star.player_id != ENV['PLAYER_ID']
-
-  attack = NP::Attack.new(
-    player: fleet.player,
-    fleet: fleet,
-    star: order.destination_star
+  client = NP::Client.new(
+    game_id: ENV['GAME_ID'],
+    auth: ENV['AUTH'],
   )
-  attack.alert
-end
+  response = client.get
 
-puts "FINISHED --- #{Time.now.utc}"
+  fleets = response.fleets.select { |f| f.player_id.to_s != ENV['PLAYER_ID'] }
+
+  fleets.each do |fleet|
+    next if fleet.orders.empty?
+
+    order = fleet.orders.first
+
+    next if order.destination_star.player_id != ENV['PLAYER_ID']
+
+    attack = NP::Attack.new(
+      player: fleet.player,
+      fleet: fleet,
+      star: order.destination_star
+    )
+    attack.alert
+  end
+
+  puts "FINISHED --- #{Time.now.utc}"
+
+rescue => e
+  Prowl.add(
+    apikey: ENV['PROWL_KEY'],
+    priority: -2,
+    application: 'Neptunes Pride - Error',
+    description: e.message
+  )
+end
